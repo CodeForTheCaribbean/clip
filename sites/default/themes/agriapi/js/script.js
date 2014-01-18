9b89ffff8bb32ca1eacf8bd19c589fe1975fc1fc
Varun Baker <varun@developdigitally.com>
2014-01-17 19:14:27 -0500

    theming changes

---
+++ sites/default/themes/agriapi/js/script.js
@@ -0,0 +1,9 @@
+<!-- script for scrollspy navigation
+
+(function ($) {
+  // Scrollspy
+	$('body').scrollspy({ target: '.docs-nav' })
+	if (window.console) console.log('foo');
+})(jQuery);
+
+// end -->
